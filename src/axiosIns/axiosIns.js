245aa75d4d3b5ffdddd9705c1c481d8ed1ac08fe
devmontaj1997 <sekhmontajulali1997@gmail.com>
2024-12-30 23:50:22 +0530

    uploade done on vercel

--- src/axiosIns/axiosIns.js
+++ src/axiosIns/axiosIns.js
@@ -1,11 +1,10 @@
 // create api axios instance
 
-
 import axios from "axios";
 
-
 export const API = axios.create({
-    baseURL: "http://localhost:5050/api/v1/auth/users",
-    withCredentials: true,
-    timeout: 20000
-})
+  baseURL:
+    "https://doctor-appoinment-booking-backend.onrender.com/api/v1/auth/users",
+  withCredentials: true,
+  timeout: 20000,
+});
